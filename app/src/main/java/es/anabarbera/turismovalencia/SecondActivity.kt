3b83b4d926b7cd1b57c2a5c22be94514ef793c1e
Ana B <anabarbera90@gmail.com>
2024-01-29 17:59:32 +0100

    primera version

---
+++ app/src/main/java/es/anabarbera/turismovalencia/SecondActivity.kt
@@ -0,0 +1,19 @@
+package es.anabarbera.turismovalencia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val imageView = findViewById<ImageView>(R.id.selectedImageView)
+
+        val imageResId = intent.getIntExtra("imageResId", 0)
+        if (imageResId !=0) {
+            imageView.setImageResource(imageResId)
+        }
+    }
+}
